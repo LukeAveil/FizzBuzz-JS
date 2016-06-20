2201bf47b0dd4cc949464b5d4570d2aa0499d838
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-06-20 17:06:53 +0100

    All tests passing

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,14 +1,32 @@
 describe("FizzBuzz", function() {
-  var game;
+  var fizzBuzz;
 
   beforeEach(function() {
     fizzBuzz = new FizzBuzz();
   });
 
-    describe("Multiple of 3 and 5", function() {
-      it("should be equal to 'fizzbuzz'", function() {
-        expect(game.play(15)).toEqual('fizzbuzz');
+  describe("Multiple of 3 and 5", function() {
+    it("should be equal to 'fizzbuzz'", function() {
+      expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
     });
+  });
 
+
+  describe("Multple of 3", function() {
+    it("should be equal to 'fizz'", function() {
+      expect(fizzBuzz.play(9)).toEqual('Fizz');
+    });
+  });
+
+  describe("Multiple of 5", function() {
+    it("should be equal to 'buzz'", function() {
+      expect(fizzBuzz.play(20)).toEqual('Buzz');
+    });
+  });
+
+  describe("Other numbers", function() {
+    it("should display the number", function() {
+      expect(fizzBuzz.play(7)).toEqual(7);
+    });
   });
 });
